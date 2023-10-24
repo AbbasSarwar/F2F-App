ac9e3ce9950aed6168906ebf56f4ab2353ba4265
Abbas Sarwar <abbassarwar100@gmail.com>
2023-10-24 18:06:00 +0500

    paragraph removed

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <section>
       <h1 className="text-3xl text-center">Home page</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit earum, tempora unde quo libero, asperiores culpa porro, odio minus voluptatibus praesentium! Assumenda eius reprehenderit veniam aperiam, repellendus dolore quae repellat!</p>
     </section>
   );
 };
