63e2fd53f40b08f22cd89117bfbbed9887a8ad62
Abbas Sarwar <abbassarwar100@gmail.com>
2023-11-03 17:12:03 +0500

    test paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <section>
       <h1 className="text-3xl text-center">Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, reprehenderit dolor dicta facere cumque non libero quo porro accusamus nostrum. Mollitia ad perferendis, accusantium animi sed hic reprehenderit fuga commodi?</p>
     </section>
   );
 };
