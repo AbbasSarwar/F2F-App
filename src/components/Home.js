a5d8720ea4b2305e06d2745eb301b0ad6451ff99
Abbas Sarwar <abbassarwar100@gmail.com>
2023-11-08 23:35:38 +0500

    reparagraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <section>
       <h1 className="text-3xl text-center">Home page</h1>
-      <p>Laboriosam, reprehenderit dolor dicta facere cumque non libero quo porro accusamus nostrum. Mollitia ad perferendis, accusantium animi sed hic reprehenderit fuga commodi?</p>
+      <p> reprehenderit dolor dicta facere cumque non libero quo porro accusamus nostrum. Mollitia ad perferendis, accusantium animi sed hic reprehenderit fuga commodi?</p>
     </section>
   );
 };
