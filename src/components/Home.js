eb2fec5f6a248eda14045f4ed72fc82fde5f778a
Abbas Sarwar <abbassarwar100@gmail.com>
2023-11-09 21:11:20 +0500

    Para graph

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ const Home = () => {
   return (
     <section>
       <h1 className="text-3xl text-center">Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, iusto suscipit? Officia vitae deserunt similique a veritatis voluptatem voluptatum! Pariatur odit assumenda repellat ea tempora corporis nesciunt eligendi in dicta.</p>
     </section>
   );
 };
